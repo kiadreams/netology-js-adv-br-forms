dbf232387eebb179cf997e6d79e0ec58d8fa40cb
Igor Kobelev <kia1809@yandex.ru>
2025-09-20 22:21:19 +0300

    added e2e tests

---
+++ e2e/popover.test.js
@@ -0,0 +1,61 @@
+import puppeteer from 'puppeteer';
+
+
+describe('Page start', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+
+  test('Form and popover should be render on the page', async () => {
+    await page.goto('http://localhost:9000/');
+
+    await page.waitForSelector('.form-example');
+  });
+
+  test('Popover should be render when clicked', async () => {
+    const text = 'А здесь размещается какой-то текстовый контент к выбранному элементу';
+    await page.goto('http://localhost:9000/');
+
+    await page.waitForSelector('.form-example');
+
+    const button = await page.$('.btn');
+    await button.click();
+    const popover = await page.waitForSelector('.popover');
+    const popoverTextContent = await popover.evaluate((popover) => {
+      const popoverBody = popover.querySelector('.popover-body');
+      return popoverBody.textContent;
+    });
+    expect(popoverTextContent).toEqual(text);
+  });
+
+  test('Change popover text content', async () => {
+    await page.goto('http://localhost:9000/');
+
+    await page.waitForSelector('.form-example');
+
+    const button = await page.$('.btn');
+    await button.evaluate((button) => {
+      button.dataset.text = 'Изменили содержимое';
+    });
+    await button.click();
+    const popover = await page.$('.popover');
+    const popoverTextContent = await popover.evaluate((popover) => {
+      const popoverBody = popover.querySelector('.popover-body');
+      return popoverBody.textContent;
+    });
+    expect(popoverTextContent).toEqual('Изменили содержимое');
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+});
--- webpack.dev.js
+++ webpack.dev.js
@@ -10,6 +10,7 @@ module.exports = merge(common, {
 
   // Spin up a server for quick development
   devServer: {
+    port: 9000,
     historyApiFallback: true,
     open: true,
     compress: true,
